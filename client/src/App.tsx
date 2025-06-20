0e038e4aedec041d1aa4cea0ff94d8bac8f98a10
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-20 21:23:58 +0530

    updated App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import HomePage from "./pages/Home"
+import LogIn from "./pages/Login"
+import SignUp from "./pages/SignUp"
 
 export default function App(){
   return(
     <>
       <HomePage/>
+      <LogIn/>
+      <SignUp />
     </>
   )
 }
