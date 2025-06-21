3a64db599ea3a41f1663bd11d29f97e4d3ff33b5
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-21 23:25:34 +0530

    added userRequired prop

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,6 +1,10 @@
-export default function NavBar({ authRequired = true }) {
+import { CircleUserRound } from 'lucide-react';
+
+export default function NavBar({ authRequired = true,userRequired=false}) {
+
+  
   return (
-    <nav className="bg-[#0c0f1a] opacity-96 shadow-md shadow-black/40 border-b border-b-white/5 w-full py-3 flex flex-row justify-between items-center px-40">
+    <nav className="bg-[#0c0f1a] opacity-96 shadow-md shadow-black/40 border-b border-b-white/5 w-full py-3 flex flex-row justify-between items-center px-10">
       <h1 className="text-xl font-semibold text-gray-200 hover:text-green-500">CodeCollab</h1>
       {authRequired && (
         <div className="flex flex-row gap-6 items-center">
@@ -12,6 +16,15 @@ export default function NavBar({ authRequired = true }) {
           </button>
         </div>
       )}
+      {
+        userRequired && (
+          <div className='flex  gap-2 items-center'>
+             <CircleUserRound size={24} />
+             <span>amishra26445</span>
+          </div>
+        )
+      }
+      
     </nav>
   );
 }
