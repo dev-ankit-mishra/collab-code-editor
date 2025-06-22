0a1cf4339fc743a73c5ec5d31c21479e0e86c735
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-22 21:01:45 +0530

    added menu hover event

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -13,37 +13,37 @@ import Button from "./Button";
 
 export default function Menu(){
   return(
-    <nav className="w-68 px-10 py-6  border-r border-r-white/10  flex flex-col justify-between">
+    <nav className="w-68 px-2  py-6  border-r border-r-white/10  flex flex-col justify-between">
           <ul className=" space-y-8">
-            <ul className="space-y-6 text-white">
-              <li className="flex items-center gap-2">
+            <ul className="space-y-5 text-gray-300">
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <Clock size={18} /> Recent
               </li>
-              <li className="flex items-center gap-2">
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <User size={18} /> My Profile
               </li>
-              <li className="flex items-center gap-2">
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <FolderGit2 size={18} /> All Repository
               </li>
-              <li className="flex items-center gap-2">
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <UserPlus size={18} /> Invite Members
               </li>
-              <li className="flex items-center gap-2">
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <Users size={18} /> Share with me
               </li>
-              <li className="flex items-center gap-2">
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <FileStack size={18} /> Templates
               </li>
-              <li className="flex items-center gap-2">
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <Bell size={18} /> Notifications
               </li>
-              <li className="flex items-center gap-2">
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <Settings size={18} /> Settings
               </li>
             </ul>
           </ul>
           
-          <Button isTransparent={true}><LogOut size={18} />
+          <Button className="mx-8" isTransparent={true}><LogOut size={18} />
               Sign out</Button>
         </nav>
   )
