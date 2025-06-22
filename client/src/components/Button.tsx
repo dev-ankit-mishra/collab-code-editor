bad57f1ce47c4284fec4e94d72ae5e5cc893f5af
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-22 20:43:54 +0530

    Added button components

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,45 @@
+import type { ReactNode } from "react";
+import type { JSX } from "react";
+// Optional but great for conditional classes
+
+type ButtonProps = {
+  children?: ReactNode;
+  isTransparent?: boolean;
+  className?: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+};
+
+export default function Button({
+  children,
+  isTransparent = false,
+  className = "",
+  onClick,
+  type = "button",
+  disabled = false,
+}: ButtonProps): JSX.Element {
+  return (
+    <>
+      {isTransparent ? (
+        <button
+          disabled={disabled}
+          onClick={onClick}
+          type={type}
+          className={`bg-transparent  border  border-green-600 hover:bg-green-600/20 py-2 px-3 text-center flex gap-2 items-center justify-center text-green-500 rounded-md hover:scale-102 shadow-md shadow-black/20 cursor-pointer transition-all duration-200 ${className} `}
+        >
+          {children}
+        </button>
+      ) : (
+        <button
+          disabled={disabled}
+          onClick={onClick}
+          type={type}
+          className={`px-3 py-2 bg-green-700 text-white rounded-md flex gap-2 justify-center items-center hover:bg-green-800 hover:scale-102 transition-all duration-200 shadow-md shadow-black/20 cursor-pointer ${className}`}
+        >
+          {children}
+        </button>
+      )}
+    </>
+  );
+}
