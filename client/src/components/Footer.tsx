fbbe7861f3a07568a9e8d184dc28d32f3f3b517e
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-22 22:20:46 +0530

    Separated foot components

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+  return(
+    <footer className="w-full bg-gray-950 text-gray-400 border-t border-t-white/5">
+        <p className="text-center py-2 text-sm  text-gray-300 tracking-wide">
+          © 2025 CodeCollab . All rights reserved .
+        </p>
+      </footer>
+  )
+}
