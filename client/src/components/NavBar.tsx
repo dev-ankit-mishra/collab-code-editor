3a41726ff0918e68012935734c4d408a714b33da
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-22 20:45:59 +0530

    updated the navbar with button components

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { CircleUserRound } from 'lucide-react';
+import Button from './Button';
 
 export default function NavBar({ authRequired = true,userRequired=false}) {
 
@@ -8,12 +9,10 @@ export default function NavBar({ authRequired = true,userRequired=false}) {
       <h1 className="text-xl font-semibold text-gray-200 hover:text-green-500">CodeCollab</h1>
       {authRequired && (
         <div className="flex flex-row gap-6 items-center">
-          <button className="rounded-lg px-4 py-2 font-medium border border-green-700 transition-all duration-200 hover:scale-102 hover:bg-green-500/20 cursor-pointer shadow-lg shadow-black/40 hover:shadow-xl">
-            Log In
-          </button>
-          <button className="rounded-lg bg-green-700 font-medium shadow-lg shadow-black/40 hover:bg-green-600 hover:scale-102 hover:shadow-xl transition-all duration-200 cursor-pointer px-4 py-2">
-            Sign Up
-          </button>
+          
+          <Button isTransparent={true}>Log In</Button>
+          <Button>Sign Up</Button>
+          
         </div>
       )}
       {
