ab178d5dc06b29ab02744bbdee32542107febb08
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-23 23:32:31 +0530

    added shareRequired prop

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,13 +1,14 @@
-import { CircleUserRound } from 'lucide-react';
+import { CircleUserRound,UserPlus } from 'lucide-react';
 import Button from './Button';
 
-export default function NavBar({ authRequired = true,userRequired=false}) {
+export default function NavBar({ authRequired = false,shareRequired=false,userRequired=false}) {
 
   
   return (
-    <nav className="bg-[#0c0f1a] opacity-96 shadow-md shadow-black/40 border-b border-b-white/5 w-full py-3 flex flex-row justify-between items-center px-10">
+    <nav className="bg-[#0c0f1a] opacity-96 shadow-md shadow-black/40 border-b border-b-white/5 w-full py-2 flex flex-row justify-between items-center px-10">
       <h1 className="text-xl font-semibold text-gray-200 hover:text-green-500">CodeCollab</h1>
-      {authRequired && (
+      <div className='flex gap-4 items-center'>
+        {authRequired && (
         <div className="flex flex-row gap-6 items-center">
           
           <Button isTransparent={true}>Log In</Button>
@@ -16,13 +17,20 @@ export default function NavBar({ authRequired = true,userRequired=false}) {
         </div>
       )}
       {
+        shareRequired && (
+          <Button isTransparent><UserPlus size={20}/> Share  </Button>
+        )
+      }
+      {
         userRequired && (
-          <div className='flex  gap-2 items-center'>
+          <div className='text-white flex  gap-2 items-center'>
              <CircleUserRound size={24} />
              <span>amishra26445</span>
           </div>
         )
       }
+      </div>
+      
       
     </nav>
   );
