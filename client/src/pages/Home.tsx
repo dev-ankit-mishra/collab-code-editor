140a429ee7e6b1abba62de3d907310eebea38466
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-23 23:33:15 +0530

    Updated the height of navbar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,7 +8,7 @@ export default function HomePage(){
     <section className="w-full min-h-screen flex flex-col text-white ">
       <NavBar authRequired={true}/>
       <main className="bg-gradient-to-b from-black via-gray-900 to-[#0c0f1a] w-full   flex-1  flex flex-col justify-center items-center px-6">
-        <h1 className="text-4xl font-bold mb-4 tracking-wide shadow-lg">Collaborative Code Editing Made Simple</h1>
+        <h1 className="text-4xl text-center font-bold mb-4 tracking-wide shadow-lg">Collaborative Code Editing Made Simple</h1>
         <p className="text-center max-w-xl mb-8 tracking-wide leading-relaxed">Write, edit and share code in real-time with your team. No setup required just open the editor and start coding.</p>
         <div className="flex gap-6">
           
