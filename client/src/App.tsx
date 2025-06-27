1fa53eb16682c351637b3a55892c6002b71c3036
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-06-27 22:44:10 +0530

    updates App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import HomePage from "./pages/Home"
 import LogIn from "./pages/Login"
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
+//import ProfilePage from "./pages/Profile"
+//import NotFoundPage from "./pages/NotFound"
+import CodeEditor from "./pages/CodeEditor"
 
 export default function App(){
   return(
@@ -10,6 +13,8 @@ export default function App(){
       <LogIn/>
       <SignUp />
       <Dashboard/>
+      <CodeEditor/>
+      
     </>
   )
 }
