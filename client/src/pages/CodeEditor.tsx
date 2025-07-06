c9795bc884ca60d0e2ccc3fd633ddba67eca3f9b
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-06 19:42:11 +0530

    added the fetch project by id

--- client/src/pages/CodeEditor.tsx
+++ client/src/pages/CodeEditor.tsx
@@ -2,11 +2,30 @@
 import NavBar from '../components/NavBar';
 import CodeArea from '../components/CodeArea';
 import SideBar from '../components/SideBar';
+import { useState,useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 export default function CodeEditor() {
 
- 
+  const {id}=useParams()
+
+ const [code,setCode]=useState<string>("Loading...")
+
+ useEffect(()=>{
+  const fetchByProject=async ()=>{
+    try{
+      const res=await fetch(`https://codevspace-aqhw.onrender.com/api/projects/${id}`)
+      const data=await res.json()
+
+      setCode(data.code || "");
+    }catch (err){
+      console.error("Failed to load project code:", err);
+      setCode("// Error loading project");
+    }
+  }
+  fetchByProject()
+ },[])
 
 
 
@@ -16,7 +35,7 @@ export default function CodeEditor() {
       <main className='w-full h-full flex- 1 flex'>
         <SideBar/>
           <div className='flex-1'>
-              <CodeArea/>
+              <CodeArea code={code}/>
              
           </div>      
       </main>
