de73c467b95f4b77d21d36ab92f204745c4ac687
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-07 17:30:02 +0530

    Added the project name span

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,12 +2,20 @@ import { CircleUserRound,UserPlus } from 'lucide-react';
 import Button from './Button';
 import {Link} from "react-router-dom"
 
-export default function NavBar({ authRequired = false,shareRequired=false,userRequired=false}) {
+import type { NavbarProp } from './Types';
+
+export default function NavBar({ authRequired = false,shareRequired=false,userRequired=false,projectName=""}:NavbarProp) {
 
   
   return (
     <nav className="bg-[#0c0f1a] opacity-96 shadow-md shadow-black/40 border-b border-b-white/5 w-full py-2 flex flex-row justify-between items-center px-10">
       <h1 className="text-xl font-semibold text-gray-200 hover:text-green-500"><Link to={"/"}>CoDevSpace</Link></h1>
+
+      <div>
+        <span>{projectName===""? null : projectName}</span>
+      </div>
+
+
       <div className='flex gap-4 items-center'>
         {authRequired && (
         <div className="flex flex-row gap-6 items-center">
@@ -17,6 +25,7 @@ export default function NavBar({ authRequired = false,shareRequired=false,userRe
           
         </div>
       )}
+      
       {
         shareRequired && (
           <Button isTransparent><UserPlus size={20}/> Share  </Button>
