22b2f07af2857fca879675e022a53751a3445433
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-07 22:00:13 +0530

    Added navLink

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,7 +1,7 @@
 import {
   Clock,
   User,
-  FolderGit2,
+  Folder,
   UserPlus,
   Users,
   FileStack,
@@ -10,21 +10,21 @@ import {
   LogOut,
 } from "lucide-react";
 import Button from "./Button";
-import {Link} from "react-router-dom"
+import {NavLink,Link} from "react-router-dom"
 
 export default function Menu(){
   return(
     <nav className="w-68 px-2  py-6  border-r border-r-white/10  flex flex-col justify-between">
           <ul className=" space-y-8">
             <ul className="space-y-5 text-gray-300">
-              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
-                <Clock size={18} /> Recent
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded cursor-pointer transition-all duration-200">
+                <NavLink to="/dashboard" end className={({isActive})=>`w-full px-8 py-2 rounded flex items-center gap-2 ${isActive ? "text-blue-500 bg-neutral-800" : "text-white"} `}><Clock size={18} /> Recent</NavLink>
               </li>
               <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <User size={18} /> My Profile
               </li>
-              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
-                <FolderGit2 size={18} /> All Repository
+              <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded  cursor-pointer transition-all duration-200">
+                <NavLink to="/dashboard/allrepository" className={({isActive})=>`w-full px-8 py-2 rounded flex items-center gap-2 ${isActive ? "text-blue-500 bg-neutral-800" : "text-white"} `}><Folder size={18} /> All Repository</NavLink>
               </li>
               <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <UserPlus size={18} /> Invite Members
