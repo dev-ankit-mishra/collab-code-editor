f54a382db88b8460b15230ec3dff525752c70c3e
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-07 22:01:05 +0530

    Updated the routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import Dashboard from "./pages/Dashboard"
 import NotFoundPage from "./pages/NotFound"
 import CodeEditor from "./pages/CodeEditor"
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import AllRepository from "./components/AllRepository"
+import Recent from "./components/Recent"
 
 export default function App(){
   return(
@@ -15,7 +17,10 @@ export default function App(){
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LogIn/>}/>
         <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}>
+          <Route index element={<Recent/>}/>
+          <Route path="allrepository" element={<AllRepository/>}  />
+        </Route>
         <Route path="/editor" element={<CodeEditor/>}/>
         <Route path="/editor/:id" element={<CodeEditor/>}/>
         <Route path="*" element={<NotFoundPage />} />
