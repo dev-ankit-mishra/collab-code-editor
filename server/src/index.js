ca0bd6c3197d976cd06660fd335564619db38bf7
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-08 18:00:07 +0530

    updated cors

--- server/src/index.js
+++ server/src/index.js
@@ -12,11 +12,6 @@ app.use(express.json());
 
 app.use(cors());
 
-app.use(cors({
-  origin: "*", // allows all domains
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-}));
 
 
 app.use("/api/projects",projectRoutes);
