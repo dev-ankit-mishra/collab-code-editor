7219603ba9cae5add677438094637aad91375bf4
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-09 00:05:36 +0530

    updated the text-color of menu

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -18,13 +18,13 @@ export default function Menu(){
           <ul className=" space-y-8">
             <ul className="space-y-5 text-gray-300">
               <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded cursor-pointer transition-all duration-200">
-                <NavLink to="/dashboard" end className={({isActive})=>`w-full px-8 py-2 rounded flex items-center gap-2 ${isActive ? "text-blue-500 bg-neutral-800" : "text-white"} `}><Clock size={18} /> Recent</NavLink>
+                <NavLink to="/dashboard" end className={({isActive})=>`w-full px-8 py-2 rounded flex items-center gap-2 ${isActive && "text-blue-500 bg-neutral-800"} `}><Clock size={18} /> Recent</NavLink>
               </li>
               <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <User size={18} /> My Profile
               </li>
               <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded  cursor-pointer transition-all duration-200">
-                <NavLink to="/dashboard/allrepository" className={({isActive})=>`w-full px-8 py-2 rounded flex items-center gap-2 ${isActive ? "text-blue-500 bg-neutral-800" : "text-white"} `}><Folder size={18} /> All Repository</NavLink>
+                <NavLink to="/dashboard/allrepository" className={({isActive})=>`w-full px-8 py-2 rounded flex items-center gap-2 ${isActive && "text-blue-500 bg-neutral-800"} `}><Folder size={18} /> All Repository</NavLink>
               </li>
               <li className="flex items-center gap-2 hover:bg-neutral-800 hover:text-white rounded px-8 py-2  cursor-pointer transition-all duration-200">
                 <UserPlus size={18} /> Invite Members
