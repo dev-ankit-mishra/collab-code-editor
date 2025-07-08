3d7908f171f68ec1480b9d3556f046ec140f0407
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-09 00:06:51 +0530

    sorted the project

--- client/src/components/Recent.tsx
+++ client/src/components/Recent.tsx
@@ -6,8 +6,16 @@ import type { DashboardOutlet } from "./Types"
 
 
 export default function Recent(){
-
   const {project,setShowModals}:DashboardOutlet=useOutletContext()
+
+  const top5Projects = project
+  .filter(p => p.updatedAt) // optional: exclude undefined timestamps
+  .sort((a, b) =>
+    new Date(b.updatedAt!).getTime() - new Date(a.updatedAt!).getTime()
+  )
+  .slice(0, 5);
+
+
   return(
     <div className="w-full flex flex-col">
           <div className=" flex justify-between p-6">
@@ -20,7 +28,7 @@ export default function Recent(){
           </div>
           <div className="flex items-center flex-wrap gap-8 p-6">
             
-            <RecentCard project={project}/>
+            <RecentCard project={top5Projects}/>
             
           </div>
         </div>
