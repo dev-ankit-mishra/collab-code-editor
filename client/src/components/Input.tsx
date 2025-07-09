fb014414124fa595c2c03cabd45b67f6ae515bcf
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-09 23:04:19 +0530

    Updated input properties

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -4,13 +4,16 @@ type InputProps={
   type?:string
   className?:string
   placeholder?:string
+  name?:string
 }
 
 
-export default function Input({type="text",className="",placeholder=""}:InputProps):JSX.Element{
+export default function Input({type,className,placeholder,name}:InputProps):JSX.Element{
   return(
     <>
       <input
+                autoComplete="off"
+                name={name}
                 type={type}
                 className={`w-full px-3 py-2 rounded-md bg-gray-800 border border-white/10  text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-blue-500 transition-all ${className}`}
                 placeholder={placeholder}
