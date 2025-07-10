0e2a3fa5c71f7aa71bd8bb07ddaca84c299e7e74
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-10 12:00:58 +0530

    updated user route

---
+++ server/src/models/UserData.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const UserSpecificSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true, // Supabase user ID — must be unique
+      index: true   // indexing for faster lookups
+    },
+    userName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    projectObject: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Project"
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+export const UserData = model("UserData", UserSpecificSchema);
