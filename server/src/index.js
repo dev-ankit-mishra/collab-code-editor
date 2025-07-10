31cec03916d6a40cc69ef6b3b6039fc32e33387f
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-10 19:00:38 +0530

    updated cors policy

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*", // or "*" for all origins (not recommended for production)
+  credentials: true,               // if using cookies or auth headers
+}));
 app.use(express.json());
 
 // Routes
