771578e224ec3f61f5d2d17f5fee52aae22f60d4
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-12 09:41:48 +0530

    Added id props

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -1,21 +1,24 @@
 import type { JSX } from "react"
 
 type InputProps={
+  id?:string
   type?:string
   className?:string
   placeholder?:string
   name?:string
+  
 }
 
 
-export default function Input({type,className,placeholder,name}:InputProps):JSX.Element{
+export default function Input({type,className,placeholder,name,id}:InputProps):JSX.Element{
   return(
     <>
       <input
+                id={id}
                 autoComplete="off"
                 name={name}
                 type={type}
-                className={`w-full px-3 py-2 rounded-md bg-gray-800 border border-white/10  text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-blue-500 transition-all ${className}`}
+                className={`w-full px-3 py-1 rounded-md bg-gray-800 border border-white/10  text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-blue-500 transition-all ${className}`}
                 placeholder={placeholder}
               />
     </>
