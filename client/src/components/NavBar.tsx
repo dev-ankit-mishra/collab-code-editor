945e61f70cb8fb15d237f9ce2834377e78e89077
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-12 09:42:38 +0530

    Added the logo

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,6 +2,7 @@ import { CircleUserRound,UserPlus,FolderOpen } from 'lucide-react';
 import Button from './Button';
 import {Link} from "react-router-dom"
 import { useAuth } from '../context/AuthContext';
+import  Logo  from '../assets/default.svg?react';
 
 import type { NavbarProp } from './Types';
 
@@ -12,8 +13,15 @@ export default function NavBar({ authRequired = false,shareRequired=false,projec
   const userId=session?.user?.email?.split("@")[0]
   
   return (
-    <nav className="bg-[#0c0f1a] opacity-96 shadow-md shadow-black/40 border-b border-b-white/5 w-full py-2 flex flex-row justify-between items-center px-10">
-      <h1 className="text-xl font-semibold text-gray-200 hover:text-green-500"><Link to={"/"}>CoDevSpace</Link></h1>
+    <nav className="bg-[#0c0f1a] opacity-96 shadow-md h-14 shadow-black/40 border-b border-b-white/5 w-full py-2 flex flex-row justify-between items-center pr-10 pl-2">
+      
+        <div className="w-52 h-full overflow-hidden flex items-center cursor-pointer pt-1">
+          <Link to="/"><Logo className="w-full h-auto" /></Link>
+        </div>
+     
+      
+
+
 
       <div>
         {
@@ -37,8 +45,8 @@ export default function NavBar({ authRequired = false,shareRequired=false,projec
           (session===undefined || session===null) ?
           (<div className="flex flex-row gap-6 items-center">
           
-          <Button isTransparent={true}><Link to={"/login"}>Log In</Link></Button>
-          <Button><Link to="/signup">Sign Up</Link></Button>
+          <Link to={"/login"}><Button isTransparent={true}>Log In</Button></Link>
+          <Link to="/signup"><Button>Sign Up</Button></Link>
         </div>) : (
           <div className='text-white flex  gap-2 items-center'>
              <CircleUserRound size={24} />
