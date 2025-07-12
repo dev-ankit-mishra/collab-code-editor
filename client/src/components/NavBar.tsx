be97fcb10bfc7e00735fba1c1444884f8b532b62
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-13 00:07:24 +0530

    improved padding

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -13,7 +13,7 @@ export default function NavBar({ authRequired = false,shareRequired=false,projec
   const userId=session?.user?.email?.split("@")[0]
   
   return (
-    <nav className="bg-[#0c0f1a] opacity-96 shadow-md h-14 shadow-black/40 border-b border-b-white/5 w-full py-2 flex flex-row justify-between items-center pr-10 pl-2">
+    <nav className="bg-[#0c0f1a] opacity-96 shadow-md h-14 shadow-black/40 border-b border-b-white/5 w-full py-2 flex flex-row justify-between items-center pr-10 ">
       
         <div className="w-52 h-full overflow-hidden flex items-center cursor-pointer pt-1">
           <Link to="/"><Logo className="w-full h-auto" /></Link>
