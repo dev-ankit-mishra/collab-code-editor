442fef494b80d1c567029cefcc61571065cc0d27
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-13 22:55:53 +0530

    improved the imports

--- client/src/routes/PublicRoute.tsx
+++ client/src/routes/PublicRoute.tsx
@@ -1,6 +1,6 @@
 // routes/PublicRoute.tsx
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/useAuth";
 
 export default function PublicRoute({ children }: { children: React.ReactNode }) {
   const { session } = useAuth();
