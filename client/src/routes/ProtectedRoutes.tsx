041adedb5700ce3032a60f962daa790af2ebb706
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-13 22:55:39 +0530

    improved the imports

--- client/src/routes/ProtectedRoutes.tsx
+++ client/src/routes/ProtectedRoutes.tsx
@@ -1,6 +1,6 @@
 // routes/ProtectedRoutes.tsx
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/useAuth";
 
 export default function ProtectedRoutes({ children }: { children: React.ReactNode }) {
   const { session } = useAuth();
