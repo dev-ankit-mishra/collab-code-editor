b66a63c99d89b886e887048e2301f8cc54639f9d
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-13 22:51:00 +0530

    Added AuthProviderContext

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import AuthContextProvider from "./context/AuthContext";
+import {AuthProvider} from "./context/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthContextProvider>
+    <AuthProvider>
       <App />
-    </AuthContextProvider>
+    </AuthProvider>
       
 
   </React.StrictMode>
