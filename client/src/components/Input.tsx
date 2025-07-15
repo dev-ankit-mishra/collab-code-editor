66f457aed0a32541796721c616bb58c2a3be4f24
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-15 22:48:47 +0530

    Added required property to input

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -18,6 +18,7 @@ export default function Input({type,className,placeholder,name,id}:InputProps):J
                 autoComplete="off"
                 name={name}
                 type={type}
+                required
                 className={`w-full px-3 py-1 rounded-md bg-gray-800 border border-white/10  text-white placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-blue-500 transition-all ${className}`}
                 placeholder={placeholder}
               />
