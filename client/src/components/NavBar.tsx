691710fa324d80238928995142e078103e4b0e67
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-15 22:47:07 +0530

    Added navOpen State

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -5,12 +5,19 @@ import { useAuth } from "../context/useAuth" ;
 import  Logo  from '../assets/default.svg?react';
 
 import type { NavbarProp } from './Types';
+import { useState,useEffect } from 'react';
 
 export default function NavBar({ authRequired = false,shareRequired=false,projectName=""}:NavbarProp) {
 
-  
+  useEffect(()=>{
+
+  },[])
+
+  const [isOpen,setIsOpen]=useState(false)
   const {session}=useAuth()
   const userId=session?.user?.email?.split("@")[0]
+
+
   
   return (
     <nav className="bg-[#0c0f1a] opacity-96 shadow-md h-14 shadow-black/40 border-b border-b-white/5 w-full py-2 flex flex-row justify-between items-center pr-10 ">
@@ -48,10 +55,22 @@ export default function NavBar({ authRequired = false,shareRequired=false,projec
           <Link to={"/login"}><Button isTransparent={true}>Log In</Button></Link>
           <Link to="/signup"><Button>Sign Up</Button></Link>
         </div>) : (
-          <div className='text-white flex  gap-2 items-center'>
+          <div className='relative flex flex-col'>
+            <div onClick={()=>setIsOpen(prev=>!prev)} className=' text-white flex  gap-2 items-center cursor-pointer'>
              <CircleUserRound size={24} />
              <span>{userId}</span>
-          </div>
+            </div>
+            {isOpen &&
+            (
+              <div className='w-fit text-sm p-1 absolute top-full mt-2 rounded bg-neutral-900'>
+                <ul className='p-1'>
+                  <li className='px-3 py-1'>Dashboard</li>
+                  <li className=''>Settings</li>
+                </ul>
+              </div>
+            )}
+          </div>  
+          
         )
         
       )}
