25dc506bd2b5313b5867b4bc63a659447ac30faa
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-17 18:43:03 +0530

    added reset password logic

--- client/src/pages/ResetPassword.tsx
+++ client/src/pages/ResetPassword.tsx
@@ -1,62 +1,69 @@
-import Button from "../components/Button";
-import Input from "../components/Input";
-import NavBar from "../components/NavBar";
-import { useState } from "react";
+import Button from "../components/Button"
+import Input from "../components/Input"
+import NavBar from "../components/NavBar"
+import { useState } from "react"
+import { useAuth } from "../context/useAuth"
 
-export default function ConfirmPassword() {
-  const [loading, setLoading] = useState();
-  const [error, setError] = useState();
-  function handleSubmit() {}
-  return (
+
+export default function ForgotPassword(){
+
+  const [loading,setLoading]=useState(false)
+  const [error,setError]=useState()
+  const {resetPassword}=useAuth()
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  e.preventDefault();
+  setLoading(true)
+  const form = e.currentTarget; // Get the form element
+  const formData = new FormData(form);
+  const email = formData.get("email") as string;
+
+  try {
+    const data = await resetPassword(email);
+
+    if (data?.error) {
+      throw new Error(data.error);
+    }
+
+  } catch (err: any) {
+    setError(err?.message || "An unexpected error occurred.");
+    console.error(err);
+  } finally{
+    setLoading(false)
+  }
+}
+
+  return(
     <section className="w-full h-screen flex flex-col">
       <NavBar authRequired={false} />
       <main className="flex-1 flex flex-col items-center text-white bg-gradient-to-b from-black via-gray-900 to-[#0c0f1a]">
         <div className="bg-black/40 border border-white/10 text-white rounded-xl shadow-2xl p-8 w-full max-w-md mx-auto mt-20">
-          <h2 className="text-2xl font-bold mb-3 text-center">
-            Set a New Password
-          </h2>
+          <h2 className="text-2xl font-bold mb-3 text-center">Reset Your Password</h2>
           <p className="text-sm text-gray-300 text-center mb-5">
-            Enter and confirm your new password to secure your account.
+            Enter your email address below and we’ll send you a link to reset your password.
           </p>
 
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <div>
-              <label
-                htmlFor="password"
-                className="block text-sm text-gray-300 mb-1"
-              >
-                Password
-              </label>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="••••••••"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="cnfpassword"
-                className="block text-sm text-gray-300 mb-1"
-              >
-                Confirm Password
-              </label>
-              <Input
-                id="cnfpassword"
-                name="password"
-                type="password"
-                placeholder="••••••••"
-              />
+              <label htmlFor="email" className="block text-sm text-gray-300 mb-1">Email</label>
+              <Input id="email" name="email" type="email" placeholder="you@example.com" />
             </div>
 
-            <Button type="submit" disabled={loading} className="mt-4">
-              {loading ? "Logging..." : "Submit"}
-            </Button>
+            <Button type="submit" disabled={loading} className="mt-4">{loading?"Submitting...":"Submit"}</Button>
           </form>
 
           {error && <p className="text-red-500 pt-4 text-center">{error}</p>}
+          
         </div>
       </main>
     </section>
-  );
+  )
 }
+
+
+
+
+
+
+
+
