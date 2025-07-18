01e0708da46fbb84c88123fba7c5b9991e9d8382
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-18 19:00:57 +0530

    made changepassword route protected

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -53,9 +53,9 @@ export default function App() {
         <Route
           path="/change-password"
           element={
-            <PublicRoute>
+            <ProtectedRoutes>
               <ChangePassword />
-            </PublicRoute>
+            </ProtectedRoutes> 
           }
         />
 
