926203418965234780c6529c03046225791e6a85
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-18 19:02:52 +0530

    Added reset password function

--- client/src/context/AuthProvider.tsx
+++ client/src/context/AuthProvider.tsx
@@ -135,7 +135,7 @@ export  function AuthProvider({ children }: { children: React.ReactNode }) {
   const resetPassword=async (email:string)=>{
     try{
       const {error}=await supabase.auth.resetPasswordForEmail(email,{
-        redirectTo:"https://codevspace.netlify.app/change-password",
+        redirectTo:"http://localhost:5173/change-password",
       })
       if(error){
         return {success:false,error:error.message}
