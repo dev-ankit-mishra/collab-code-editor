537cac376db38ed71583f090d12321ecf2580050
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-22 18:39:07 +0530

    Added spinner loader to run button

--- client/src/components/Output.tsx
+++ client/src/components/Output.tsx
@@ -1,19 +1,21 @@
 import Button from "./Button"
-import type { JSX } from "react"
+import {type JSX } from "react"
 import { Play } from 'lucide-react';
+
 
 
 type OutputProps={
   onClick : ()=>void
   children : string
   bgClass : string
+  isLoading: boolean
 }
 
 
-export default function Output({onClick,children,bgClass}:OutputProps):JSX.Element{
+export default function Output({onClick,children,bgClass,isLoading}:OutputProps):JSX.Element{
   return(
     <main className="w-[34rem] h-full p-4 flex flex-col gap-4">
-      <Button className="w-fit px-3" onClick={onClick}><Play size={16}/> Run</Button>
+      <Button className="h-8 px-3 w-20" onClick={onClick} disabled={isLoading}>{isLoading ? (<div className='w-5 h-5 rounded-full border-2 border-white border-t-transparent animate-spin'/>) : <><Play size={16}/> Run</>}</Button>
       <div className={`flex-1 p-4 rounded-xl border border-white/10 shadow-[0_0_15px_rgba(255,255,255,0.05)] ${bgClass}`}>
           {children}
       </div>
