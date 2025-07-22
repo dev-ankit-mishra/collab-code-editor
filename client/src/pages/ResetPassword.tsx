cc1b19805ef4adce03a1a89d36463921c01f3ff6
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-22 18:40:56 +0530

    Added spinner loader to submit button

--- client/src/pages/ResetPassword.tsx
+++ client/src/pages/ResetPassword.tsx
@@ -50,7 +50,7 @@ export default function ResetPassword(){
               <Input id="email" name="email" type="email" placeholder="you@example.com" />
             </div>
 
-            <Button type="submit" disabled={loading} className="mt-4">{loading?"Submitting...":"Submit"}</Button>
+            <Button type="submit" disabled={loading} className="mt-4 h-8">{loading?(<div className='w-5 h-5 rounded-full border-2 border-white border-t-transparent animate-spin'/>):"Submit"}</Button>
           </form>
 
           {}
