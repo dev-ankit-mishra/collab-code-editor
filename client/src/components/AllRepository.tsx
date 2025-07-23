7335daaa41d6fa276411704a4c76b71f040aff20
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-23 19:23:56 +0530

    extracted handleDelete function from outlet context

--- client/src/components/AllRepository.tsx
+++ client/src/components/AllRepository.tsx
@@ -3,7 +3,7 @@ import { useOutletContext } from "react-router-dom"
 import type { DashboardOutlet } from "./Types"
 
 export default function AllRepository(){
-  const {project}:DashboardOutlet=useOutletContext()
+  const {project,handleDelete}:DashboardOutlet=useOutletContext()
   const latestProjects = project
   .filter(p => p.updatedAt) // optional: exclude undefined timestamps
   .sort((a, b) =>
@@ -16,7 +16,7 @@ export default function AllRepository(){
           
           <div className="flex items-center flex-wrap gap-8 p-6">
             
-            <RecentCard project={latestProjects}/>
+            <RecentCard project={latestProjects} onDelete={handleDelete}/>
             
           </div>
         </div>
