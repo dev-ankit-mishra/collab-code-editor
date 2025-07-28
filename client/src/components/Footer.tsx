fc351886e7c47cc47f968accc544d797372fea63
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-28 20:02:21 +0530

    Added contact information

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,12 @@
 export default function Footer(){
   return(
-    <footer className="w-full bg-gray-950 text-gray-400 border-t border-t-white/5">
-        <p className="text-center py-2 text-sm  text-gray-300 tracking-wide">
-          © 2025 CodeCollab . All rights reserved .
-        </p>
+    <footer className="w-full bg-gray-950 flex items-center justify-between px-6 text-gray-400 border-t border-t-white/5">
+        <span className="text-center py-2 text-sm  text-gray-400 tracking-wide">
+         Copyright © 2025 CoDevSpace
+        </span>
+        <a href="mailto:contact@ankitmishra.pro" className="text-center py-2 text-sm   text-gray-400 tracking-wide cursor-pointer">
+          contact@ankitmishra.pro
+        </a>
       </footer>
   )
 }
