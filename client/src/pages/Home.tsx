22868b5a8cab87141fcfbae3ec465edcc6195c50
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-28 19:50:06 +0530

    Added features component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,7 +2,9 @@ import { ArrowRight } from 'lucide-react';
 import NavBar from '../components/NavBar';
 import Button from "../components/Button"
 import Footer from '../components/Footer';
+import Features from '../components/Features';
 import {useNavigate} from "react-router-dom"
+
 import { useAuth } from '../context/useAuth';
 
 export default function HomePage(){
@@ -26,9 +28,9 @@ export default function HomePage(){
   }
 
   return(
-    <section className="w-full min-h-screen flex flex-col text-white ">
+    <section className="w-full flex flex-col text-white ">
       <NavBar authRequired={true}/>
-      <main className="bg-gradient-to-b from-black via-gray-900 to-[#0c0f1a] w-full   flex-1  flex flex-col justify-center items-center px-6">
+      <main className="bg-gradient-to-b from-black via-gray-900 to-[#0c0f1a] w-full h-[95vh] flex flex-col justify-center items-center px-6">
         <h1 className="text-4xl text-center font-bold mb-4 tracking-wide shadow-lg">Collaborative Code Editing Made Simple</h1>
         <p className="text-center max-w-xl mb-8 tracking-wide leading-relaxed">Write, edit and share code in real-time with your team. No setup required just open the editor and start coding.</p>
         <div className="flex gap-6">
@@ -37,6 +39,7 @@ export default function HomePage(){
           <Button isTransparent={true} className='px-4 py-2' onClick={getStartedBtn}>Get Started  <ArrowRight size={20}/></Button>
         </div>
       </main>
+      <Features/>
       <Footer/>
     </section>
 
