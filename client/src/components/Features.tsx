5d6ebb9150961b78f64476ed7368c67c10448330
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-28 21:56:28 +0530

    improved padding

--- client/src/components/Features.tsx
+++ client/src/components/Features.tsx
@@ -30,18 +30,18 @@ export default function Features() {
   return (
     <main className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-950 to-slate-900 py-16 px-4 text-white">
       <div className="max-w-7xl mx-auto text-center">
-        <h2 className="text-3xl md:text-4xl font-bold">
+        <h2 className="text-2xl md:text-3xl mt-4 font-bold">
           Collaborate, Interview, and Learn—All in One Place
         </h2>
         <div className="w-20 h-1 bg-gradient-to-r from-blue-300 via-blue-500 to-blue-600  rounded-lg mx-auto mt-8 mb-12 hover:w-28 transition duration-300" />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 justify-items-center pt-20">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 justify-items-center pt-22">
           {features.map(({ id, title, description, icon }) => (
             <div
               key={id}
               className="bg-slate-900 border border-white/10 rounded-2xl w-full max-w-sm p-5 hover:shadow-lg transition"
             >
-              <div className="mb-4 flex items-center justify-center">{icon}</div>
+              <div className="mb-3 flex items-center justify-center">{icon}</div>
               <h3 className="text-xl font-semibold text-center mb-3">{title}</h3>
               <p className="text-white/80 text-[15px] text-center tracking-wide leading-relaxed">{description}</p>
             </div>
