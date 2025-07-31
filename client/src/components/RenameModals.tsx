9d5d90e7d060425c0fdff0ecf6272335ed48250e
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-07-31 19:06:15 +0530

    Added Rename Functionality

--- client/src/components/RenameModals.tsx
+++ client/src/components/RenameModals.tsx
@@ -1,18 +1,44 @@
 import Input from "./Input";
 import Button from "./Button";
 import { BiRename } from "react-icons/bi";
+import { useAuth } from "../context/useAuth";
+import axios from "axios";
+import type { FormEvent } from "react";
+
 
 type RenameModalsProp = {
   setOpen: (val: boolean) => void;
   id:string | undefined
+  onRename:(id:string,name:string)=>void
 };
 
-export default function RenameModals({ setOpen,id }: RenameModalsProp) {
+export default function RenameModals({ setOpen,id,onRename }: RenameModalsProp) {
 
+  const {session}=useAuth();
+async function renameProject(e: FormEvent<HTMLFormElement>) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  const name = formData.get("name")?.toString().trim();
+  const userId = session?.user.id;
 
-  async function handleSubmit(){
-    console.log(id)
+  if (!id || !userId || !name) {
+    return;
   }
+
+  try {
+    await axios.put(
+      `https://codevspace-aqhw.onrender.com/api/projects/${userId}/${id}`,
+      { projectName: name }
+    );
+    console.log("renamed successfully");
+    onRename(id,name)
+  } catch (err) {
+    console.log(err);
+  } finally {
+    setOpen(false);
+  }
+}
+
 
   return (
     <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center">
@@ -29,18 +55,13 @@ export default function RenameModals({ setOpen,id }: RenameModalsProp) {
 
         {/* Form */}
         <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            // handle rename logic here
-            handleSubmit()
-            setOpen(false);
-          }}
+          onSubmit={renameProject}
           className="space-y-4"
         >
           <label htmlFor="name" className="block text-white font-medium">
             Enter New Project Name
           </label>
-          <Input id="name" type="text" placeholder="New project name" />
+          <Input id="name" name="name" type="text" placeholder="New project name" />
 
           <Button type="submit" className="w-full flex items-center justify-center gap-2">
             <BiRename size={16} />
