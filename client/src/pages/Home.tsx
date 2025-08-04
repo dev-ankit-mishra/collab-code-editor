6ea8f0cb6b0b959503cae9065656ecfd8bd00b0b
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-08-04 12:55:35 +0530

    Added Contact Section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,6 +6,7 @@ import Features from '../components/Features';
 import {useNavigate} from "react-router-dom"
 
 import { useAuth } from '../context/useAuth';
+import Contact from '../components/Contact';
 
 export default function HomePage(){
   const {session}=useAuth()
@@ -30,7 +31,7 @@ export default function HomePage(){
   return(
     <section className="w-full flex flex-col text-white ">
       <NavBar authRequired={true}/>
-      <main className="bg-gradient-to-b from-black via-gray-900 to-[#0c0f1a] w-full h-[95vh] flex flex-col justify-center items-center px-6 pb-8">
+      <main className="bg-gradient-to-b from-black via-gray-900 to-[#0c0f1a] w-full min-h-screen flex flex-col justify-center items-center px-6 pb-8">
         <h1 className="text-4xl text-center font-bold mb-4 tracking-wide shadow-lg">Collaborative Code Editing Made Simple</h1>
         <p className="text-center max-w-xl mb-8 tracking-wide leading-relaxed">Write, edit and share code in real-time with your team. No setup required just open the editor and start coding.</p>
         <div className="flex gap-6">
@@ -40,6 +41,7 @@ export default function HomePage(){
         </div>
       </main>
       <Features/>
+      <Contact/>
       <Footer/>
     </section>
 
