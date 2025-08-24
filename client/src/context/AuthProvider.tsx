9cca56bf388f8c01551f8618140bbad07290a9fa
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-08-24 22:23:07 +0530

    updated the redirect of google and github signin

--- client/src/context/AuthProvider.tsx
+++ client/src/context/AuthProvider.tsx
@@ -107,7 +107,7 @@ export  function AuthProvider({ children }: { children: React.ReactNode }) {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: "http://codevspace.netlify.app/dashboard",
+          redirectTo: "https://codevspace.netlify.app/dashboard",
         },
       });
       if (error) return { success: false, error: error.message };
@@ -122,7 +122,7 @@ export  function AuthProvider({ children }: { children: React.ReactNode }) {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "github",
         options: {
-          redirectTo: "http://codevspace.netlify.app/dashboard",
+          redirectTo: "https://codevspace.netlify.app/dashboard",
         },
       });
       if (error) return { success: false, error: error.message };
