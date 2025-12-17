2151a8d7c9f7162d3d3e5962f84e90f7eb113091
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-12-17 12:02:09 +0530

    updated the type of projectObject

--- client/src/components/Api.ts
+++ client/src/components/Api.ts
@@ -4,20 +4,22 @@ const API = axios.create({
   baseURL: "https://emkc.org/api/v2/piston",
 });
 
-
-
-export const executeCode = async ({label,version}:{
-    label: string
-    version: string
-  },sourceCode: string) => {
+export const executeCode = async (
+  { label, version }: { label: string; version: string },
+  sourceCode: string,
+  input: string
+) => {
   const response = await API.post("/execute", {
     language: label.toLowerCase(),
-    version: version, 
+    version,
     files: [
       {
+        name: "main",
         content: sourceCode,
       },
     ],
+    stdin: input,
   });
+
   return response.data;
 };
