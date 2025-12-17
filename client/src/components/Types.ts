6388888e512133126f30067da6e81e94f031e232
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-12-17 12:05:37 +0530

    Added projectObject type

--- client/src/components/Types.ts
+++ client/src/components/Types.ts
@@ -27,7 +27,7 @@ export type ModalProps={
 export type ProjectDetails={
   _id?: string;
   projectName:string,
-  userId:string,
+  userId?:string,
   code:string,
   template:Language,
   createdAt?:Date,
@@ -47,23 +47,57 @@ export type DashboardOutlet={
 }
 
 
+export type codeAreaProps = {
+  projectObject: ProjectDetails;
+  accessRole: "OWNER" | "EDITOR" | "VIEWER";
+};
 
-export type codeAreaProps={
-  projectObject:ProjectDetails
+export type AuthContextType = {
+  session: Session | null;
+  loading:boolean;
+  signInUser: (
+    email: string,
+    password: string
+  ) => Promise<{
+    success: boolean;
+    error?: string;
+    data?: any;
+  }>;
 
-}
+  signUpUser: (
+    name: string,
+    email: string,
+    password: string
+  ) => Promise<{
+    success: boolean;
+    error?: string;
+    data?: any;
+  }>;
 
-export type AuthContextType ={
-  session: Session | null | undefined;
-  signInUser: (email: string, password: string) => Promise<{ success: boolean; error?: string; data?: any }>;
-  signOutUser: () => Promise<{ success: boolean; error?: string }>;
-  signUpUser: (name:string,email:string,password:string)=>Promise<{success:boolean,error?:string,data?:any}>;
-  signInWithGoogle:()=>Promise<{success:boolean,error?:string}>;
-  signInWithGithub:()=>Promise<{success:boolean,error?:string}>;
-  resetPassword:(email:string)=>Promise<{success:boolean,error?:string}>;
-  updateUser:(password:string)=>Promise<{success:boolean,error?:string}>;
+  signOutUser: () => Promise<{
+    success: boolean;
+    error?: string;
+  }>;
 
-}
+  signInWithGoogle: () => Promise<void>;
+  signInWithGithub: () => Promise<void>;
+
+  resetPassword: (
+    email: string
+  ) => Promise<{
+    success: boolean;
+    error?: string;
+  }>;
+
+  updateUser: (
+    password: string
+  ) => Promise<{
+    success: boolean;
+    error?: string;
+  }>;
+};
+
+
 
 export type MenuProps={
   setShowModals:(val:boolean)=>void
