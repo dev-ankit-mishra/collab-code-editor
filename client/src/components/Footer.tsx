0c7e57e92ad7de596d63ba20cd339cffea3c5592
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-12-26 17:57:53 +0530

    Added github link

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,23 @@
+import { FaGithub } from "react-icons/fa";
+
+
 export default function Footer(){
   return(
     <footer className="w-full bg-gray-950 flex items-center justify-between px-6 text-gray-400 border-t border-t-white/5">
         <span className="text-center py-2 text-sm  text-gray-400 tracking-wide">
          Copyright © 2025 CoDevSpace
         </span>
+  <a
+    href="https://github.com/dev-ankit-mishra/collab-code-editor"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="flex items-center gap-2 hover:text-white"
+  >
+    <FaGithub size={16} />
+    Contribute on GitHub
+  </a>
+
+
         <a href="mailto:developer.ankitmishra@gmail.com" className="text-center py-2 text-sm   text-gray-400 tracking-wide cursor-pointer">
           contact@ankitmishra.pro
         </a>
