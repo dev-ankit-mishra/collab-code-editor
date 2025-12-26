e87c875a4fc0cc1cc1b3ad3f155ebd859c53b8d8
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-12-26 18:02:14 +0530

    Added setting item component

--- client/src/components/SettingItem.tsx
+++ client/src/components/SettingItem.tsx
@@ -6,11 +6,13 @@ export default function SettingItem({
   description,
   actionLabel,
   danger = false,
+  settingFunction
 }: {
   title: string;
   description: string;
   actionLabel: string;
   danger?: boolean;
+  settingFunction: ()=> void
 }) {
   return (
     <div className="flex items-center justify-between p-4 rounded-lg bg-gray-900 border border-white/5">
@@ -30,6 +32,7 @@ export default function SettingItem({
             ? "text-red-500 border-red-500 hover:bg-red-500/10"
             : ""
         }
+        onClick={settingFunction}
       >
         {actionLabel}
       </Button>
